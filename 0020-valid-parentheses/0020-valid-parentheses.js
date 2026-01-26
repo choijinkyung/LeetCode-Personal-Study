3b184de58c7be1a0048e9fc3acdd60b3d30e19de
j_iky <twin7014@naver.com>
2026-01-26 13:08:49 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const closed = new Map([
+        [')','('],['}','{'],[']','[']
+    ])
+    const stack = [];
+
+    for(let char of s){
+        if(closed.has(char)){
+            // 닫는 괄호인데 스택이 비어있으면 ❌
+            if(stack.length == 0){
+                return false;
+            }
+            const top =  stack.pop()
+             // 짝이 안 맞으면 ❌
+            if(top !== closed.get(char)) return false;
+        }else{
+              // 여는 괄호
+            stack.push(char)
+        }
+    }
+    return true;
+};
